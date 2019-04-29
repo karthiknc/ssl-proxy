c20e7c23e6143d632380478582ffb45582921208
Karthik Bhat <karthik.bhat@news.co.uk>
2019-04-29 17:01:57 +0530

    Certificate expiry 10 years

--- add_self_signed_certs.sh
+++ add_self_signed_certs.sh
@@ -13,12 +13,12 @@ mkdir -p $CERT_DIR
 # Generate the root CA if it doesn't exist
 if [ ! -f ${CERT_DIR}/rootCA.crt ]; then
 	openssl genrsa -out ${CERT_DIR}/rootCA.key 2048
-	openssl req -x509 -new -nodes -key ${CERT_DIR}/rootCA.key -sha256 -days 1024 -subj "/C=IN/ST=Bengaluru/L=ECity/O=News/CN=ecslocal" -out ${CERT_DIR}/rootCA.crt
+	openssl req -x509 -new -nodes -key ${CERT_DIR}/rootCA.key -sha256 -days 3650 -subj "/C=IN/ST=Bengaluru/L=ECity/O=News/CN=ecslocal" -out ${CERT_DIR}/rootCA.crt
 fi
 
 if [ ! -f ${CERT_DIR}/key.pem ]; then
     # Generate the certificate
     openssl genrsa -out ${CERT_DIR}/key.pem 2048
     openssl req -new -sha256 -key ${CERT_DIR}/key.pem -nodes -subj "/C=IN/ST=Bengaluru/L=ECity/O=NTS/CN=${DOMAIN}" -out ${CERT_DIR}/csr.pem
-    openssl x509 -req -in ${CERT_DIR}/csr.pem -CA ${CERT_DIR}/rootCA.crt -CAkey ${CERT_DIR}/rootCA.key -CAcreateserial -out ${CERT_DIR}/cert.pem -sha256 -extfile ${V3_DIR}v3.ext
+    openssl x509 -req -in ${CERT_DIR}/csr.pem -CA ${CERT_DIR}/rootCA.crt -CAkey ${CERT_DIR}/rootCA.key -CAcreateserial -out ${CERT_DIR}/cert.pem -sha256 -extfile ${V3_DIR}v3.ext -days 3650
 fi
